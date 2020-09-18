93f9e6ff34e90332a21fb0d27670d127195374f7
TimepieceMaster <jacvill@att.net>
2020-09-18 10:02:24 -0500

    Added cube interactiosn

---
+++ VirtualRealityCS6334/CS6334/Assets/Scripts/CubeInteractions.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInteractions : MonoBehaviour
+{
+	private void Update()
+	{
+		TryToMove();
+		TryToRotate();
+	}
+
+	bool moving = false;
+	bool rotating = false;
+
+	public void StartMoving()
+	{
+		moving = true;
+	}
+
+	public void StopMoving()
+	{
+		moving = false;
+	}
+
+	public void StartRotating()
+	{
+		rotating = true;
+	}
+
+	public void StopRotating()
+	{
+		rotating = false;
+	}
+
+    public void TryToMove()
+	{
+		if (moving)
+		{
+			transform.position = transform.position + Vector3.left * 0.5f * Time.deltaTime;
+		}
+	}
+
+	public void TryToRotate()
+	{
+		if (rotating)
+		{
+			transform.Rotate(Vector3.up, 180.0f * Time.deltaTime);
+		}
+	}
+
+	public void TurnRed()
+	{
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+}
